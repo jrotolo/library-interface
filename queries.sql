d511c17219682b8623f6097a47354e19dfc4e4a6
Jarrod <jrotol5@lsu.edu>
2014-12-01 23:06:54 -0600

    added my queries

--- queries.sql
+++ queries.sql
@@ -101,3 +101,144 @@ WHERE Authors.name = 'Alysson Stark';
 |      25 | Madilyn Swaniawski   |      84 | Synergized secondary matrices     | 
 +---------+----------------------+---------+-----------------------------------+
 /*
+
+/* Find all users who have a rental */
+SELECT DISTINCT Users.user_id, Users.name
+FROM Users NATURAL JOIN Rental;
+
+/*
++---------+----------------------+
+| user_id | name                 |
++---------+----------------------+
+|       1 | Reid Kuphal          |
+|       3 | Miss Angelina Kertzm |
+|       4 | Lue Pfannerstill     |
+|       5 | Mrs. Odessa Blanda   |
+|       6 | Ashtyn Bruen         |
+|       7 | Marcelino Bins       |
+|       9 | Delpha Wiza          |
+|      10 | Ford Stoltenberg     |
+|      11 | Monty Gutmann        |
+|      12 | Dahlia Larkin V      |
+|      13 | Lulu Olson           |
+|      14 | Mr. Declan Rosenbaum |
+|      15 | Alvina Ryan          |
+|      16 | Warren Toy           |
+|      17 | Emma Mosciski        |
+|      18 | Shanelle Dare        |
+|      19 | Dean Turcotte        |
+|      20 | Adela Ratke          |
+|      21 | Dorcas Bode          |
+|      22 | Tommie Deckow V      |
+|      24 | Grant Borer          |
+|      25 | Madilyn Swaniawski   |
+|      26 | Donavon Walsh DDS    |
+|      27 | Brody Marks          |
+|      28 | Gladys Johnston      |
+|      29 | Alec Dibbert         |
+|      30 | Demetris Stiedemann  |
+|      31 | Cleo Cassin          |
+|      32 | Melvina Schmeler     |
+|      33 | Wilfred King V       |
+|      35 | Zena Terry           |
++---------+----------------------+
+*/
+
+/* Find all unique author and publisher pairs */
+SELECT r1.author_id, Authors.name as author_name, r1.publisher_id, Publishers.name as publisher_name
+FROM (SELECT DISTINCT author_id, publisher_id
+FROM Books) as r1, Authors, Publishers
+WHERE r1.author_id=Authors.author_id AND r1.publisher_id=Publishers.publisher_id;
+
+/*
++-----------+----------------------+--------------+--------------------------------+
+| author_id | author_name          | publisher_id | publisher_name                 |
++-----------+----------------------+--------------+--------------------------------+
+|         2 | Breanne Murazik      |            7 | Vandervort, Ferry and Moore    |
+|         2 | Breanne Murazik      |            5 | Klein Press                    |
+|         2 | Breanne Murazik      |            9 | Torp-Reichert                  |
+|         2 | Breanne Murazik      |           10 | Wisozk Printers                |
+|         3 | Miss Monica Klein    |            3 | Dickens-Stehr                  |
+|         3 | Miss Monica Klein    |            7 | Vandervort, Ferry and Moore    |
+|         3 | Miss Monica Klein    |           10 | Wisozk Printers                |
+|         3 | Miss Monica Klein    |            9 | Torp-Reichert                  |
+|         5 | Shyanne Jacobs       |            1 | Weber-Halvorson                |
+|         5 | Shyanne Jacobs       |            7 | Vandervort, Ferry and Moore    |
+|         5 | Shyanne Jacobs       |            5 | Klein Press                    |
+|         6 | Reuben Kunze         |            9 | Torp-Reichert                  |
+|         6 | Reuben Kunze         |            3 | Dickens-Stehr                  |
+|         6 | Reuben Kunze         |           10 | Wisozk Printers                |
+|         6 | Reuben Kunze         |            5 | Klein Press                    |
+|         6 | Reuben Kunze         |            6 | Berge Press                    |
+|         7 | Christopher Hirthe   |            7 | Vandervort, Ferry and Moore    |
+|         9 | Miss Isabella Legros |            6 | Berge Press                    |
+|         9 | Miss Isabella Legros |            7 | Vandervort, Ferry and Moore    |
+|         9 | Miss Isabella Legros |            9 | Torp-Reichert                  |
+|         9 | Miss Isabella Legros |            1 | Weber-Halvorson                |
+|         9 | Miss Isabella Legros |            4 | Parker-Cormier                 |
+|         9 | Miss Isabella Legros |            3 | Dickens-Stehr                  |
+|        10 | Kaya Kunze Jr.       |            4 | Parker-Cormier                 |
+|        10 | Kaya Kunze Jr.       |            1 | Weber-Halvorson                |
+|        10 | Kaya Kunze Jr.       |            9 | Torp-Reichert                  |
+|        10 | Kaya Kunze Jr.       |            8 | Lemke-Vandervort               |
+|        10 | Kaya Kunze Jr.       |            7 | Vandervort, Ferry and Moore    |
+|        10 | Kaya Kunze Jr.       |            6 | Berge Press                    |
+|        11 | Sigrid Herzog IV     |            5 | Klein Press                    |
+|        11 | Sigrid Herzog IV     |            8 | Lemke-Vandervort               |
+|        11 | Sigrid Herzog IV     |            4 | Parker-Cormier                 |
+|        12 | Jannie Emard         |            2 | Schowalter, Ernser and Koelpin |
+|        12 | Jannie Emard         |            4 | Parker-Cormier                 |
+|        12 | Jannie Emard         |           10 | Wisozk Printers                |
+|        12 | Jannie Emard         |            6 | Berge Press                    |
+|        12 | Jannie Emard         |            5 | Klein Press                    |
+|        14 | Mossie Langworth     |            1 | Weber-Halvorson                |
+|        14 | Mossie Langworth     |            4 | Parker-Cormier                 |
+|        14 | Mossie Langworth     |           10 | Wisozk Printers                |
+|        14 | Mossie Langworth     |            3 | Dickens-Stehr                  |
+|        14 | Mossie Langworth     |            6 | Berge Press                    |
+|        15 | Izabella Bartell I   |            4 | Parker-Cormier                 |
+|        15 | Izabella Bartell I   |            5 | Klein Press                    |
+|        15 | Izabella Bartell I   |            7 | Vandervort, Ferry and Moore    |
+|        15 | Izabella Bartell I   |            9 | Torp-Reichert                  |
+|        16 | Verlie Haag          |            1 | Weber-Halvorson                |
+|        17 | Hallie Wiegand       |            2 | Schowalter, Ernser and Koelpin |
+|        17 | Hallie Wiegand       |            8 | Lemke-Vandervort               |
+|        17 | Hallie Wiegand       |            6 | Berge Press                    |
+|        17 | Hallie Wiegand       |            9 | Torp-Reichert                  |
+|        19 | Roderick Eichmann Sr |            4 | Parker-Cormier                 |
+|        19 | Roderick Eichmann Sr |            7 | Vandervort, Ferry and Moore    |
+|        21 | Adelbert Corwin      |            3 | Dickens-Stehr                  |
+|        21 | Adelbert Corwin      |            7 | Vandervort, Ferry and Moore    |
+|        21 | Adelbert Corwin      |            2 | Schowalter, Ernser and Koelpin |
+|        21 | Adelbert Corwin      |           10 | Wisozk Printers                |
+|        21 | Adelbert Corwin      |            6 | Berge Press                    |
+|        22 | Alysson Stark        |            9 | Torp-Reichert                  |
+|        22 | Alysson Stark        |            2 | Schowalter, Ernser and Koelpin |
+|        22 | Alysson Stark        |            4 | Parker-Cormier                 |
+|        23 | Julian Wyman         |            6 | Berge Press                    |
+|        23 | Julian Wyman         |            4 | Parker-Cormier                 |
+|        23 | Julian Wyman         |            5 | Klein Press                    |
+|        24 | Ms. Bethany Sawayn   |            7 | Vandervort, Ferry and Moore    |
+|        24 | Ms. Bethany Sawayn   |            9 | Torp-Reichert                  |
+|        25 | Mrs. Kody Kohler     |            3 | Dickens-Stehr                  |
+|        25 | Mrs. Kody Kohler     |            8 | Lemke-Vandervort               |
+|        25 | Mrs. Kody Kohler     |            7 | Vandervort, Ferry and Moore    |
++-----------+----------------------+--------------+--------------------------------+
+*/
+
+
+/* Find all users who have checked out a book in the past n days */
+SELECT user_id, book_id, checkout_date 
+FROM Rentals 
+WHERE datediff(curdate(), checkout_date) <= 7;
+
+/*
++---------+---------+---------------------+
+| user_id | book_id | checkout_date       |
++---------+---------+---------------------+
+|       3 |      58 | 2014-11-30 00:00:00 |
+|      16 |      10 | 2014-11-28 00:00:00 |
+|      18 |      87 | 2014-11-28 00:00:00 |
+|      26 |      22 | 2014-11-25 00:00:00 |
++---------+---------+---------------------+
+*/
