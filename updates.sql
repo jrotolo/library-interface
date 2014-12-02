72c9bbc47c8c059287013bbb507180a73153fb18
Jonathan Hooper <Jonathan@newaperio.com>
2014-12-01 23:48:27 -0600

    Update updates.sql

--- updates.sql
+++ updates.sql
@@ -1,14 +1,24 @@
 /* Change rental from active to inactive */
-update Rental
+UPDATE Rental
 	SET active = 0
 	WHERE rental_id = 3;
 
 /* Change book title */
-update Books
-	SET title = "Pygmalion"
+UPDATE Books
+  SET title = "Pygmalion"
 	WHERE title = "Pigmalion"
 
 /* Change user name */
-update Users
-	SET name = "John"
-	WHERE name = "Jon"
+UPDATE Users
+  SET name = "John Snow"
+  WHERE name = "Jon Snow"
+
+/* Update a fee's balance */
+UPDATE Fees
+  SET balance = 200
+  WHERE fee_id = 15;
+
+/* Change rental due date */
+UPDATE Rentals
+  SET due_date = TIMESTAMP '2015-01-01'
+  WHERE rental_id = 10;
